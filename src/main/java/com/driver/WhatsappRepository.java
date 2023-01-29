7d378bc5b38fca219d728ca224dda4ac2a8f6a8d
mani <mani>
2023-01-29 18:02:58 +0530

    message

---
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -0,0 +1,9 @@
+package com.driver;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WhatsappRepository {
+
+
+}
---
+++ src/main/java/com/driver/WhatsappService.java
@@ -0,0 +1,7 @@
+package com.driver;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WhatsappService {
+}
